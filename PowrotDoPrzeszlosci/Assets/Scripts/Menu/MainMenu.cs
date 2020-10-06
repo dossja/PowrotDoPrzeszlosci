98c78bc03ed7c992f54caae8f68af1c9c541e965
Dossja <domka_24@interia.pl>
2020-10-06 19:08:38 +0200

    Basic main menu functionality.

---
+++ PowrotDoPrzeszlosci/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // New game ->  Load first level (Scene with ID 1)
+    public void PlayNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Continue game ->  Load level with saved ID
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Wyjscie z gry!");
+        Application.Quit();
+    }
+}
